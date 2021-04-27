63bb4ef1d8d3b7d3db99848d3bd14a53f31e4c44
Vitaliy <v.a.grahov@ya.ru>
2021-04-28 02:36:42 +0500

    настройка первого фрагмента

--- app/src/main/java/com/example/gb_2_06_fragments/MainActivity.java
+++ app/src/main/java/com/example/gb_2_06_fragments/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.gb_2_06_fragments;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    boolean isLandscape = getResources().getBoolean(R.bool.isLandscape);
+    FragmentManager fragmentManager = getSupportFragmentManager();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (isLandscape) {
+
+        } else {
+            Fragment fragment = fragmentManager.findFragmentById(R.id.container);
+            if (fragment == null) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.container, new ListFragment())
+                        .commit();
+            }
+        }
     }
 }
