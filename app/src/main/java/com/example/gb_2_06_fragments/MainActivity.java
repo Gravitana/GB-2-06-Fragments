1271afb43740f248bed0eae25b5985dd544734c8
gravitana <v.a.grahov@ya.ru>
2021-04-28 15:57:09 +0500

    Начальная конфигурация фрагментов

--- app/src/main/java/com/example/gb_2_06_fragments/MainActivity.java
+++ app/src/main/java/com/example/gb_2_06_fragments/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.gb_2_06_fragments;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
@@ -10,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        boolean isLandscape = getResources().getBoolean(R.bool.isLandscape);
+
+        if (!isLandscape) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+
+            Fragment fragment = fragmentManager.findFragmentById(R.id.container);
+
+            if (fragment == null) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.container, new ListFragment())
+                        .commit();
+            }
+        }
     }
 }
