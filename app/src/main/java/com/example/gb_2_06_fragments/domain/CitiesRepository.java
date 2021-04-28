83d31e5d49914449a5c8462382090fce6bb2d7ab
gravitana <v.a.grahov@ya.ru>
2021-04-28 22:40:52 +0500

    создан ресурс городов

--- app/src/main/java/com/example/gb_2_06_fragments/domain/CitiesRepository.java
+++ app/src/main/java/com/example/gb_2_06_fragments/domain/CitiesRepository.java
@@ -1,5 +1,7 @@
 package com.example.gb_2_06_fragments.domain;
 
+import com.example.gb_2_06_fragments.R;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,7 +9,13 @@ public class CitiesRepository {
 
     public List<City> getCities() {
         ArrayList<City> cities = new ArrayList<>();
-
+        cities.add(new City(R.drawable.izh, R.string.izh));
+        cities.add(new City(R.drawable.msc, R.string.msc));
+        cities.add(new City(R.drawable.nsk, R.string.nsk));
+        cities.add(new City(R.drawable.sam, R.string.sam));
+        cities.add(new City(R.drawable.ebrg, R.string.ebrg));
+        cities.add(new City(R.drawable.spb, R.string.spb));
+        cities.add(new City(R.drawable.chel, R.string.chel));
         return cities;
     }
 }
