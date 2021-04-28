606958193c279617681ff0b1be2b26c76e466666
gravitana <v.a.grahov@ya.ru>
2021-04-28 16:34:29 +0500

    недоделка

---
+++ app/src/main/java/com/example/gb_2_06_fragments/domain/CitiesRepositiry.java
@@ -0,0 +1,13 @@
+package com.example.gb_2_06_fragments.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CitiesRepositiry {
+
+    public List<City> getCities() {
+        ArrayList<City> cities = new ArrayList<>();
+
+        return cities;
+    }
+}
