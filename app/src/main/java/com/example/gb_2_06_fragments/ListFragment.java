2736cf94ecf66979221a017af8a98c0f676aa774
Vitaliy <v.a.grahov@ya.ru>
2021-04-28 23:25:27 +0500

    Сформирован список городов

--- app/src/main/java/com/example/gb_2_06_fragments/ListFragment.java
+++ app/src/main/java/com/example/gb_2_06_fragments/ListFragment.java
@@ -9,6 +9,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.gb_2_06_fragments.domain.CitiesRepository;
+import com.example.gb_2_06_fragments.domain.City;
+
+import java.util.List;
 
 public class ListFragment extends Fragment {
 
@@ -27,6 +36,21 @@ public class ListFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        List<City> cities = new CitiesRepository().getCities();
 
+        LinearLayout citiesList = (LinearLayout) view;
+
+        for (City city : cities) {
+            View cityView = LayoutInflater.from(requireContext())
+                    .inflate(R.layout.item_city, citiesList, false);
+
+            ImageView image = cityView.findViewById(R.id.image);
+            image.setImageResource(city.getDrawableRes());
+
+            TextView title = cityView.findViewById(R.id.city_name);
+            title.setText(city.getTitleRes());
+
+            citiesList.addView(cityView);
+        }
     }
 }
--- app/src/main/java/com/example/gb_2_06_fragments/domain/CitiesRepository.java
+++ app/src/main/java/com/example/gb_2_06_fragments/domain/CitiesRepository.java
@@ -10,12 +10,12 @@ public class CitiesRepository {
     public List<City> getCities() {
         ArrayList<City> cities = new ArrayList<>();
         cities.add(new City(R.drawable.izh, R.string.izh));
+        cities.add(new City(R.drawable.chel, R.string.chel));
         cities.add(new City(R.drawable.msc, R.string.msc));
         cities.add(new City(R.drawable.nsk, R.string.nsk));
         cities.add(new City(R.drawable.sam, R.string.sam));
         cities.add(new City(R.drawable.ebrg, R.string.ebrg));
         cities.add(new City(R.drawable.spb, R.string.spb));
-        cities.add(new City(R.drawable.chel, R.string.chel));
         return cities;
     }
 }
